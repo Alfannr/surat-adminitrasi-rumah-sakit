ea65ba9708061ed00e3549baf14615ac5a6deb2d
Alfannr <alfann812@gmail.com>
2022-08-18 16:05:39 +0700

    upload

---
+++ application/models/Model_surat_keluar.php
@@ -0,0 +1,73 @@
+<?php
+
+class Model_surat_keluar extends CI_Model {
+
+    function lihat(){
+        $query="select s.*, k.nama as kategori, u.nama as user
+                from surat_keluar as s
+                join kategori_surat as k
+                on s.id_kat_surat=k.id_kat_surat
+                join user as u
+                on s.id_user=u.id_user";
+        return $this->db->query($query);
+    }
+    
+    function lihat2($username){
+        $query="select s.*, k.nama as kategori, u.nama as user, u.id_user 
+                from surat_keluar as s
+                join kategori_surat as k
+                on s.id_kat_surat=k.id_kat_surat
+                join user as u
+                on s.id_user=u.id_user
+                where u.username = '$username'";
+        return $this->db->query($query);
+    }
+    
+    function kategori_surat(){
+        $query="select * from kategori_surat";
+        return $this->db->query($query);
+    }
+    
+    function user(){
+        $query="select * from user";
+        return $this->db->query($query);
+    }
+    
+    
+    function tambah($data){
+        $this->db->insert('surat_keluar',$data);
+    }
+    
+    function get_one($id){
+        $param = array('id'=>$id);
+        return $this->db->get_where('surat_keluar',$param);
+    }
+    
+    function edit($data,$id)
+    {
+        $this->db->where('nomer_surat',$id);
+        $this->db->update('surat_keluar',$data);
+    }
+    
+
+    
+    function hapus($id){
+        $this->db->where('id',$id);
+        $this->db->delete('surat_keluar');
+        
+    }
+    
+    function cetak($tgl_awal,$tgl_akhir,$username){
+        $query="select s.*, k.nama as kategori
+                from surat_keluar as s
+                join kategori_surat as k
+                on s.id_kat_surat=k.id_kat_surat
+                join user as u
+                on s.id_user=u.id_user
+                where u.username = '$username' and tgl_keluar between '$tgl_awal' and '$tgl_akhir'";
+        return $this->db->query($query);
+    }
+    
+
+
+}
